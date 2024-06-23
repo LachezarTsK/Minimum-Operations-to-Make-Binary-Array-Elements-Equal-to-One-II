24593a73503f740c234324b48dff53eb4734a041
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-23 14:51:50 +0200

    Update Solution.go

--- Solution.go
+++ Solution.go
@@ -4,13 +4,13 @@ package main
 import "fmt"
 
 func minOperations(input []int) int {
-	flip := 0
-	minOperations := 0
-	for i := range input {
-		if (input[i] ^ flip) == 0 {
-			minOperations++
-			flip = flip ^ 1
-		}
-	}
-	return minOperations
+    flip := 0
+    minOperations := 0
+    for i := range input {
+        if (input[i] ^ flip) == 0 {
+            minOperations++
+            flip = flip ^ 1
+        }
+    }
+    return minOperations
 }
