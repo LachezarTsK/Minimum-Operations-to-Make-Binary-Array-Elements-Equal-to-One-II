b3c73b4fa5403dc59ddd28c202a54a3aed920296
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-23 14:19:28 +0200

    Create Solution.go

---
+++ Solution.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "fmt"
+
+func minOperations(input []int) int {
+	flip := 0
+	minOperations := 0
+	for i := range input {
+		if (input[i] ^ flip) == 0 {
+			minOperations++
+			flip = flip ^ 1
+		}
+	}
+	return minOperations
+}
